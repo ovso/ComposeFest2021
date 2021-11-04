1b788dca4baba11990fcd2475fe1f3e014def631
jaeho <ovsoce@gmail.com>
2021-11-05 00:38:23 +0900

    feat: 패딩 넣음

--- week 1-Jetpack Compose basics/MyBasicCodeLab/app/src/main/java/io/github/ovso/compose/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyBasicCodeLab/app/src/main/java/io/github/ovso/compose/MainActivity.kt
@@ -3,11 +3,14 @@ package io.github.ovso.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.ovso.compose.ui.theme.MyBasicCodeLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +29,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
 }
 
 @Preview(showBackground = true)
