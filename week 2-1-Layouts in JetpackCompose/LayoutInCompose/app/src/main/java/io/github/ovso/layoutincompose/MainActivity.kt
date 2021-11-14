18f4048905f5f2529ac330037b78d23ed1660420
jaeho <ovsoce@gmail.com>
2021-11-14 20:51:50 +0900

    feat: 알프레드 시슬리 아이템 구현

--- week 2-1-Layouts in JetpackCompose/LayoutInCompose/app/src/main/java/io/github/ovso/layoutincompose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutInCompose/app/src/main/java/io/github/ovso/layoutincompose/MainActivity.kt
@@ -3,11 +3,17 @@ package io.github.ovso.layoutincompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.ovso.layoutincompose.ui.theme.LayoutInComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,5 +41,34 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     LayoutInComposeTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun PhotographerCard() {
+    Row {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(
+                alpha = 0.2f
+            )
+        ) {
+
+        }
+        Column {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minute ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    LayoutInComposeTheme {
+        PhotographerCard()
     }
 }
