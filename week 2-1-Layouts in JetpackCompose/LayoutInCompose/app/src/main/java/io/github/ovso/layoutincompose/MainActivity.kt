d6c275da121f469ee1daadfa8bb78dbe49ea88c5
jaeho <ovsoce@gmail.com>
2021-11-16 23:25:29 +0900

    feat: 리스트 스크롤 버튼 구현

--- week 2-1-Layouts in JetpackCompose/LayoutInCompose/app/src/main/java/io/github/ovso/layoutincompose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutInCompose/app/src/main/java/io/github/ovso/layoutincompose/MainActivity.kt
@@ -3,12 +3,10 @@ package io.github.ovso.layoutincompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
@@ -18,22 +16,86 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import io.github.ovso.layoutincompose.ui.theme.LayoutInComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutInComposeTheme {
-                SimpleList()
+                ScrollingList()
             }
         }
+    }
+}
+
+@Composable
+fun ScrollingList() {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row {
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem(0)
+                    }
+                }
+            ) {
+                Text(text = "Scroll to the top")
+            }
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem(listSize - 1)
+                    }
+                }
+            ) {
+                Text(text = "Scroll to the end")
+            }
+        }
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageList() {
+    val scrollState = rememberLazyListState()
+
+    LazyColumn(state = scrollState) {
+        items(100) {
+            ImageListItem(it)
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png",
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp),
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
     }
 }
 
@@ -81,9 +143,19 @@ fun BodyContent(modifier: Modifier) {
 @Composable
 fun LayoutsCodelabPreview() {
     LayoutInComposeTheme {
+        ImageListItem(1)
+    }
+}
+
+/*
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    LayoutInComposeTheme {
         SimpleList()
     }
 }
+*/
 
 @Composable
 fun PhotographerCard(modifier: Modifier = Modifier) {
@@ -119,10 +191,11 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
     }
 }
 
+/*
 @Preview
 @Composable
 fun PhotographerCardPreview() {
     LayoutInComposeTheme {
         PhotographerCard()
     }
-}
+}*/
