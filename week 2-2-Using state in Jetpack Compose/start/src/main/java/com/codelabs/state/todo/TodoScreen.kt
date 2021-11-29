8b042a60a98e73167aef24c973a396151b5d9dc5
jaeho <ovsoce@gmail.com>
2021-11-29 23:34:00 +0900

    feat: remember 로 iconAlpha 기억하기

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
@@ -93,7 +93,10 @@ fun TodoRow(todo: TodoItem, onItemClicked: (TodoItem) -> Unit, modifier: Modifie
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(todo.task)
-        val iconAlpha = randomTint()
+//        val iconAlpha: Float = remember(todo.id) { randomTint() }
+//        val iconAlpha = randomTint()
+        val iconAlpha: Float = remember(todo.id) { randomTint()}
+
         Icon(
             imageVector = todo.icon.imageVector,
             tint = LocalContentColor.current.copy(alpha = iconAlpha),
