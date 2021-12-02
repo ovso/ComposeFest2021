49b3aaef827ee3df2c5df1fbdbe1fc97cf803b65
jaeho <ovsoce@gmail.com>
2021-12-02 23:11:38 +0900

    feat: 몇몇 이벤트 추가

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
@@ -16,7 +16,10 @@
 
 package com.codelabs.state.todo
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class TodoViewModel : ViewModel() {
@@ -26,11 +29,33 @@ class TodoViewModel : ViewModel() {
     var todoItems = mutableStateListOf<TodoItem>()
         private set
 
+    private var currentEditPosition by mutableStateOf(-1)
+
+    val currentEditItem: TodoItem?
+        get() = todoItems.getOrNull(currentEditPosition)
+
     fun addItem(item: TodoItem) {
         todoItems.add(item)
     }
 
     fun removeItem(item: TodoItem) {
         todoItems.remove(item)
+        onEditDone()
+    }
+
+    fun onEditItemSelected(item: TodoItem) {
+        currentEditPosition = todoItems.indexOf(item)
+    }
+
+    fun onEditDone() {
+        currentEditPosition = -1
+    }
+
+    fun onEditItemChange(item: TodoItem) {
+        val currentItem = requireNotNull(currentEditItem)
+        require(currentItem.id == item.id) {
+            "currentEditItem 과 동일한 ID를 가진 항목만 변경할 수 있습니다."
+        }
+        todoItems[currentEditPosition] = item
     }
 }
