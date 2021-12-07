d8e30e888eae89e484035f5486101b243df06f6b
jaeho <ovsoce@gmail.com>
2021-12-07 12:21:24 +0900

    feat: 딥링에 반응하기 위한 구현

--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -30,6 +30,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.example.compose.rally.data.UserData
 import com.example.compose.rally.ui.accounts.AccountsBody
 import com.example.compose.rally.ui.accounts.SingleAccountBody
@@ -111,7 +112,10 @@ fun RallyApp() {
                         navArgument("name") {
                             type = NavType.StringType
                         }
-                    )
+                    ),
+                    deepLinks = listOf(navDeepLink {
+                        uriPattern = "rally://$accountName/{name}"
+                    })
                 ) { entry ->
                     val accName = entry.arguments?.getString("name")
                     val account = UserData.getAccount(accName)
