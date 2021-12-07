2e52a2631e196ac5c4e0fc468bc2524853be5eb5
jaeho <ovsoce@gmail.com>
2021-12-07 22:17:51 +0900

    test: rallyTopAppBarTest 구현

---
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -0,0 +1,27 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import com.example.compose.rally.ui.theme.RallyTheme
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun rallyTopAppBarTest() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTheme {
+                RallyTopAppBar(
+                    allScreens = allScreens,
+                    onTabSelected = {},
+                    currentScreen = RallyScreen.Accounts
+                )
+            }
+        }
+        Thread.sleep(5000)
+    }
+}
